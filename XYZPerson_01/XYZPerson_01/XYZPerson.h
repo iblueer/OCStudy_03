1642809cf934237c4c868e014788a73c0f58b230
Maemolee <iblueer@live.cn>
2015-07-05 01:02:10 +0800

    今天开始用慕课网上的教程学oc

---
+++ XYZPerson_01/XYZPerson_01/XYZPerson.h
@@ -0,0 +1,16 @@
+//
+//  XYZPerson.h
+//  XYZPerson_01
+//
+//  Created by Maemolee on 15/7/4.
+//  Copyright (c) 2015年 Maemolee. All rights reserved.
+//
+
+#ifndef XYZPerson_01_XYZPerson_h
+#define XYZPerson_01_XYZPerson_h
+
+@interface XYZPerson: NSObject
+- (void)sayHello;
+@end
+
+#endif
---
+++ XYZPerson_01/XYZPerson_01/main.m
@@ -0,0 +1,21 @@
+//
+//  main.m
+//  XYZPerson_01
+//
+//  Created by Maemolee on 15/7/4.
+//  Copyright (c) 2015年 Maemolee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "XYZPerson.h"
+
+int main(int argc, const char * argv[]) {
+
+    @autoreleasepool {
+        NSLog(@"This is my 1st string.");
+        NSLog(@"This is my 2nd string.");
+        NSLog(@"This is my 3rd string.");
+        NSLog(@"This is my 4th string.");
+    }
+    return 0;
+}
